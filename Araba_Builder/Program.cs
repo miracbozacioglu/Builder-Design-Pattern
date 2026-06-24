9bc1151f1f46057034aa77700afac997f718364a
miracbozacioglu <mbozacioglu06@gmail.com>
2026-06-24 22:03:41 +0300

    Add project files.

---
+++ Araba_Builder/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Araba_Builder
+{
+    public class Araba
+    {
+        public string Renk { get; set; }
+        public int Hp { get; set; }
+        public string Vites { get; set; }
+
+        public string Yakit { get; set; }
+
+        public Araba()
+        {
+            Renk = "Beyaz";
+            Hp = 75;
+            Vites = "Manuel";
+            Yakit = "Benzin";
+        }
+    }
+
+    public class ArabaBuilder
+    {
+        private Araba araba;
+
+        public ArabaBuilder()
+        {
+            araba = new Araba();
+        }
+
+        public ArabaBuilder Setrenk(string renk)
+        {
+            araba.Renk = renk;
+            return this;
+        }
+
+        public ArabaBuilder SetHp(int hp)
+        {
+            araba.Hp = hp;
+            return this;
+        }
+        public ArabaBuilder SetVites(string vites)
+        {
+            araba.Vites = vites;
+            return this;
+        }
+        public ArabaBuilder SetYakit(string yakit)
+        {
+            araba.Yakit = yakit;
+            return this;
+        }
+        public Araba Build()
+        {
+            return araba;
+        }
+
+    }
+
+    public class ArabaDirector
+    {
+        public Araba EkoSinifAraba()
+        {
+            return new ArabaBuilder()
+                .Setrenk("SimliBeyaz")
+                .SetHp(100)
+                .SetVites("Yarı-Otomatik")
+                .SetYakit("Dizel")
+                .Build();
+        }
+
+        public Araba OrtaSinifAraba()
+        {
+            return new ArabaBuilder()
+                .Setrenk("Kırmızı")
+                .SetHp(120)
+                .SetVites("Otomatik")
+                .SetYakit("Benzin")
+                .Build();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba ortasinif = new ArabaDirector().OrtaSinifAraba();
+
+            var mycar = new ArabaBuilder()
+                .Setrenk("Mavi")
+                .SetHp(150)
+                .SetVites("Otomatik")
+                .SetYakit("Dizel")
+                .Build();
+
+            Console.WriteLine(mycar.Hp);
+
+
+            var director = new ArabaDirector();
+            var mycar2 = director.OrtaSinifAraba();
+        }
+    }
+
+  
+
+}
---
+++ Pizza_Builder/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Pizza_Builder
+{
+    public class Pizza
+    {
+        public string Boyut { get; set; }
+        public string HamurTipi { get; set; }
+        public bool Peynir { get; set; }
+        public bool Zeytin { get; set; }
+
+        public bool Mantar { get; set; }
+    }
+
+    public class PizzaBuilder
+    {
+        private Pizza pizza;
+
+        public PizzaBuilder()
+        {
+            pizza = new Pizza();
+        }
+
+        public PizzaBuilder SetBoyut(string boyut)
+        {
+            pizza.Boyut = boyut;
+            return this;
+        }
+        public PizzaBuilder SetHamurTipi(string hamurTi)
+        {
+            pizza.HamurTipi = hamurTi;
+            return this;
+        }
+        public PizzaBuilder SetPeynir()
+        {
+            pizza.Peynir = true;
+            return this;
+        }
+        public Pizza Build()
+        {
+            return pizza;
+        }
+    }
+
+    public class PizzaDirector
+    {
+        public Pizza OrtaPizza()
+        {
+            return new PizzaBuilder()
+                .SetBoyut("Orta")
+                .SetHamurTipi("Kalın")
+                .SetPeynir()
+                .Build();
+        }
+
+        public Pizza KarisikPizza()
+        {
+            return new PizzaBuilder()
+                .SetBoyut("Buyuk")
+                .SetHamurTipi("İnce")
+                .SetPeynir()
+                .Build();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ortapizza = new PizzaDirector().OrtaPizza();
+
+            var mypizza = new PizzaBuilder()
+                .SetBoyut("cmkefs")
+                .SetHamurTipi("debds")
+                .SetPeynir()
+                .Build();
+
+
+            var director = new PizzaDirector();
+            var orta = director.OrtaPizza();
+
+        }
+    }
+
+}
